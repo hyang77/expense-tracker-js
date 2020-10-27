c7b1d422170e3c2e1e04775f879bbb157b769042
Haoli Yang <hyang77@hawk.iit.edu>
2020-10-27 15:13:20 -0500

    Create expense routes

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// include routes
+const expenseRoutes = require('./routes/expense')
+
+// expense routes
+app.use('/expense', expenseRoutes)
 
 app.get('/', (req, res) => {
   res.send('Got a get request!')
