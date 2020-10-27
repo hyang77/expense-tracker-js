47b9dff2f067e443a8c443a3e05834dad3e2396a
Haoli Yang <hyang77@hawk.iit.edu>
2020-10-27 16:45:02 -0500

    Update expense routes

--- routes/expense.js
+++ routes/expense.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 
-// view all expenses
+// get all expenses
 router.get('/', function (req, res, next) {
     res.send('Got a get request for all expenses!')
 })
@@ -11,15 +11,15 @@ router.get('/:expenseId', function (req, res, next) {
     res.send('Got a get request for an expense!')
 })
 // create an expense
-router.post('/:expenseId', function (req, res, next) {
+router.post('/add', function (req, res, next) {
     res.send('Got a post request for an expense!')
 })
 // edit an expense
-router.patch('/:expenseId', function (req, res, next) {
+router.patch('/edit/:expenseId', function (req, res, next) {
     res.send('Got a patch request for an expense!')
 })
 // delete an expense
-router.delete('/:expenseId', function (req, res, next) {
+router.delete('/delete/:expenseId', function (req, res, next) {
     res.send('Got a delete request for an expense!')
 })
 
