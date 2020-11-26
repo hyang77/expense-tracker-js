5757250d7a82fc7c829d4e17fc65c476c00a7725
Haoli Yang <hyang77@hawk.iit.edu>
2020-11-25 22:32:00 -0600

    Handle CORS errors

--- server.js
+++ server.js
@@ -8,6 +8,19 @@ const expenseRoutes = require('./routes/expense')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// Handle CORS
+app.use((req, res, next) => {
+  res.header('Acess-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers',"Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Methods',
+          'PUT, POST, PATCH, DELETE, GET'
+      );
+      return res.status(200).json({});
+  }
+  next(); 
+})
+
 // expense routes
 app.use('/expense', expenseRoutes)
 app.use(morgan('dev'));
